4da58b3c06b20022979cafbdcb1426d478c6d021
Michail Vourlakos <mvourlakos@gmail.com>
2018-11-09 10:46:29 +0200

    update schemecolors and some licenses at files

--- libappletdecoration/previewbridge.h
+++ libappletdecoration/previewbridge.h
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef KDECORATIONS_PREVIEW_BRIDGE_H
 #define KDECORATIONS_PREVIEW_BRIDGE_H
 
